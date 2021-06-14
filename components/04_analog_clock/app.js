dff3578cbec271efe18ef27a8867436664e609a4
dooyeong20 <noviced22@gmail.com>
2021-06-14 17:17:08 +0900

    feat: implemented project 04

--- components/04_analog_clock/app.js
+++ components/04_analog_clock/app.js
@@ -1,9 +1,37 @@
-// constant
+// state
+
+let timeDegree = 0;
 
 // doms
 
-// utility
+const $hour = document.querySelector('.hour');
+const $min = document.querySelector('.minute');
+const $sec = document.querySelector('.second');
 
-// DOM manipulation
+const convertTimeToTotalDegree = date =>
+  (date.getHours() * 3600 + date.getMinutes() * 60 + date.getSeconds()) * 6;
+
+const render = () => {
+  $hour.style.setProperty('--deg', timeDegree / 720);
+  $min.style.setProperty('--deg', timeDegree / 60);
+  $sec.style.setProperty('--deg', timeDegree);
+};
+
+const setTimeDegree = _timeDegree => {
+  timeDegree = _timeDegree;
+  render();
+};
+
+const fetch = () => {
+  setTimeDegree(convertTimeToTotalDegree(new Date()));
+};
 
 // Event Listener
+
+window.addEventListener('DOMContentLoaded', () => {
+  fetch();
+
+  setInterval(() => {
+    setTimeDegree(timeDegree + 6);
+  }, 1000);
+});
